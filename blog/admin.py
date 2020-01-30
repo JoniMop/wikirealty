1e62e14d6343e729b27b0769a5ffe7b8764409f5
stcr <jonatangurari@gmail.com>
2020-01-30 17:35:56 -0600

    Adding ability to make posts from the administrator interface.

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Post 
+#importo el modelo Post que cree
 
 # Register your models here.
+admin.site.register(Post)
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class Post(models.Model):
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    #creo la tabla user en donde va registrando os usuarios
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
+
+
+#Abajo creo funcion para que el dia en que se crea y el dia en que se publica puedan ser diferentes
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+
+#represento todo lo creado en string
+    def __str__(self):
+        return self.title
+
