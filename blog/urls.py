41872ef03c79ad8a5bc291f29f57cf6c6cf8f540
stcr <jonatangurari@gmail.com>
2020-02-01 22:02:56 -0600

    Add fueature to edit blog post from frontend

--- blog/urls.py
+++ blog/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     #localhost/post/new   
     path('post/new/', views.post_new, name='post_edit'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
 ]
